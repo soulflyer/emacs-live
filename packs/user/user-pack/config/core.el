1366a4892d4d9cb56b3f0601b7a0c64d968802d9
Iain <public@soulflyer.co.uk>
2014-10-30 17:42:35 +0700

    Added web mode

--- packs/user/user-pack/config/core.el
+++ packs/user/user-pack/config/core.el
@@ -293,3 +293,14 @@ and overlay is highlighted between MK and END-MK."
   (message "major mode %s" major-mode)
   (if (string= major-mode "dired-mode")
       (revert-buffer)))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))

291e90962a2bc354249510c633559672d43f45d1
iain <iain@soulflyer.co.uk>
2018-09-01 15:10:01 +0700

    make fancy chars work with clojurescript as well as clojure

--- packs/stable/clojure-pack/config/clojure-conf.el
+++ packs/stable/clojure-pack/config/clojure-conf.el
@@ -29,6 +29,27 @@
                                                (match-end 1) "∈")
                                nil))))))
 
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojurescript-mode `(("(\\(fn\\)[\[[:space:]]"
+                           (0 (progn (compose-region (match-beginning 1)
+                                                     (match-end 1) "λ")
+                                     nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojurescript-mode `(("\\(#\\)("
+                           (0 (progn (compose-region (match-beginning 1)
+                                                     (match-end 1) "ƒ")
+                                     nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojurescript-mode `(("\\(#\\){"
+                           (0 (progn (compose-region (match-beginning 1)
+                                                     (match-end 1) "∈")
+                                     nil))))))
+
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
 

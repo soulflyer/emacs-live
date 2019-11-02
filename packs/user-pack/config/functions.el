18664d4f3bcf3ce017173aa532caf06ad8466df0
iain <iain@soulflyer.co.uk>
2019-11-02 09:23:02 +0700

    added .tmux and sketch of fn to set env vars for data42

--- packs/user-pack/config/functions.el
+++ packs/user-pack/config/functions.el
@@ -19,4 +19,20 @@
     (call-interactively 'v-resize))
    (t (push key unread-command-events))))
 
+(defun read-hana-environment (config-file)
+  ;; This will source the .rc file and add any environment variables there to process-environment.
+  ;; It's intended purpose is switching hana configs without having to restart the emacs daemon.
+  ;; It WON'T add any env vars that contain spaces in the value. This means that D42ONE_GC_OPTS
+  ;; won't get added, but that is unlikely to be of use from within emacs.
+  (with-temp-buffer
+    (call-process "bash" nil t nil "-c"
+                  (concat "source " config-file "; env | grep 'D42' | grep -v ' '"))
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let ((current-line (split-string (buffer-substring (point) (line-end-position)) "=")))
+        (setenv (first current-line) (second current-line)))
+      (forward-line 1))))
+
+(defun read-hana ()
+  (read-hana-environment "/Users/iain/Code/Flexiana/Data-42/d42one-release-conf/d42one-conf.d/d42one.rc"))
 ;;; functions.el ends here

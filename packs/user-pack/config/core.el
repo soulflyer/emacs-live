c24f09ecb95b44e8d794daf917fd83984f2a4456
iain <iain@soulflyer.co.uk>
2020-04-20 01:28:11 +0700

    added touch of colour to mode-line innactive for remote machines

--- packs/user-pack/config/core.el
+++ packs/user-pack/config/core.el
@@ -6,7 +6,8 @@
 (require 'iy-go-to-char)
 
 (unless (string= "subversion\n" (shell-command-to-string "hostname -s"))
-  (set-face-attribute 'mode-line nil :foreground "#C930d0"))
+  (set-face-attribute 'mode-line nil :foreground "#C930d0")
+  (set-face-attribute 'mode-line-inactive nil :foreground "#9080a0"))
 
 ;;(global-aggressive-indent-mode 1)
 ;; (add-to-list 'aggressive-indent-excluded-modes 'cider-repl-mode)
